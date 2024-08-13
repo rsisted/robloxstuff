7d6b38278b6d49e648a496accab8aa64cd9358e5
rsisted <axtorisounds@gmail.com>
2024-08-13 14:51:12 +0200

    Create rsloader.lua

---
+++ rsloader.lua
@@ -0,0 +1,198 @@
+-- Instances
+
+local RSPackLoader = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local TextLabel = Instance.new("TextLabel")
+local TextLabel_2 = Instance.new("TextLabel")
+local TextLabel_3 = Instance.new("TextLabel")
+local TextLabel_4 = Instance.new("TextLabel")
+local TextLabel_5 = Instance.new("TextLabel")
+local TextLabel_6 = Instance.new("TextLabel")
+local Frame_2 = Instance.new("Frame")
+local UICorner_2 = Instance.new("UICorner")
+local TextLabel_7 = Instance.new("TextLabel")
+local TextButton = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local TextButton_2 = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+
+-- Properties
+
+RSPackLoader.Name = "RSPackLoader"
+RSPackLoader.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+Frame.Parent = RSPackLoader
+Frame.BackgroundColor3 = Color3.new(0.223529, 0.223529, 0.223529)
+Frame.BorderColor3 = Color3.new(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.303412467, 0, 0.294486225, 0)
+Frame.Size = UDim2.new(0, 529, 0, 328)
+
+UICorner.Parent = Frame
+UICorner.CornerRadius = UDim.new(0, 11)
+
+TextLabel.Parent = Frame
+TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel.BackgroundTransparency = 1
+TextLabel.BorderColor3 = Color3.new(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.0226843096, 0, 0.0182926822, 0)
+TextLabel.Size = UDim2.new(0, 67, 0, 22)
+TextLabel.Font = Enum.Font.ArialBold
+TextLabel.Text = "<RSISTED>"
+TextLabel.TextColor3 = Color3.new(1, 1, 1)
+TextLabel.TextScaled = true
+TextLabel.TextSize = 14
+TextLabel.TextWrapped = true
+TextLabel.TextXAlignment = Enum.TextXAlignment.Left
+
+TextLabel_2.Parent = Frame
+TextLabel_2.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel_2.BackgroundTransparency = 1
+TextLabel_2.BorderColor3 = Color3.new(0, 0, 0)
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.Position = UDim2.new(0.217391297, 0, 0.0182926822, 0)
+TextLabel_2.Size = UDim2.new(0, 299, 0, 31)
+TextLabel_2.Font = Enum.Font.ArialBold
+TextLabel_2.Text = "Script-Pack Loader"
+TextLabel_2.TextColor3 = Color3.new(1, 1, 1)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 14
+TextLabel_2.TextWrapped = true
+
+TextLabel_3.Parent = Frame
+TextLabel_3.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel_3.BackgroundTransparency = 1
+TextLabel_3.BorderColor3 = Color3.new(0, 0, 0)
+TextLabel_3.BorderSizePixel = 0
+TextLabel_3.Position = UDim2.new(0.134215504, 0, 0.112804875, 0)
+TextLabel_3.Size = UDim2.new(0, 388, 0, 14)
+TextLabel_3.Font = Enum.Font.ArialBold
+TextLabel_3.Text = "Version 1.0"
+TextLabel_3.TextColor3 = Color3.new(1, 1, 1)
+TextLabel_3.TextScaled = true
+TextLabel_3.TextSize = 14
+TextLabel_3.TextWrapped = true
+
+TextLabel_4.Parent = Frame
+TextLabel_4.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel_4.BackgroundTransparency = 1
+TextLabel_4.BorderColor3 = Color3.new(0, 0, 0)
+TextLabel_4.BorderSizePixel = 0
+TextLabel_4.Position = UDim2.new(0.0226843096, 0, 0.189024389, 0)
+TextLabel_4.Size = UDim2.new(0, 299, 0, 31)
+TextLabel_4.Font = Enum.Font.ArialBold
+TextLabel_4.Text = "The following scripts will be activated:"
+TextLabel_4.TextColor3 = Color3.new(1, 1, 1)
+TextLabel_4.TextScaled = true
+TextLabel_4.TextSize = 14
+TextLabel_4.TextWrapped = true
+TextLabel_4.TextXAlignment = Enum.TextXAlignment.Left
+
+TextLabel_5.Parent = Frame
+TextLabel_5.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel_5.BackgroundTransparency = 1
+TextLabel_5.BorderColor3 = Color3.new(0, 0, 0)
+TextLabel_5.BorderSizePixel = 0
+TextLabel_5.Position = UDim2.new(0.0226843096, 0, 0.283536583, 0)
+TextLabel_5.Size = UDim2.new(0, 299, 0, 19)
+TextLabel_5.Font = Enum.Font.ArialBold
+TextLabel_5.Text = "Infinite Yield"
+TextLabel_5.TextColor3 = Color3.new(1, 1, 1)
+TextLabel_5.TextScaled = true
+TextLabel_5.TextSize = 14
+TextLabel_5.TextWrapped = true
+TextLabel_5.TextXAlignment = Enum.TextXAlignment.Left
+
+TextLabel_6.Parent = Frame
+TextLabel_6.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel_6.BackgroundTransparency = 1
+TextLabel_6.BorderColor3 = Color3.new(0, 0, 0)
+TextLabel_6.BorderSizePixel = 0
+TextLabel_6.Position = UDim2.new(0.0226843096, 0, 0.341463417, 0)
+TextLabel_6.Size = UDim2.new(0, 299, 0, 19)
+TextLabel_6.Font = Enum.Font.ArialBold
+TextLabel_6.Text = "Incognito Chat Bypasser"
+TextLabel_6.TextColor3 = Color3.new(1, 1, 1)
+TextLabel_6.TextScaled = true
+TextLabel_6.TextSize = 14
+TextLabel_6.TextWrapped = true
+TextLabel_6.TextXAlignment = Enum.TextXAlignment.Left
+
+Frame_2.Parent = Frame
+Frame_2.BackgroundColor3 = Color3.new(1, 1, 1)
+Frame_2.BorderColor3 = Color3.new(0, 0, 0)
+Frame_2.BorderSizePixel = 0
+Frame_2.Position = UDim2.new(-0.000935372431, 0, 0.18168138, 0)
+Frame_2.Size = UDim2.new(0, 529, 0, 3)
+
+UICorner_2.Parent = Frame_2
+UICorner_2.CornerRadius = UDim.new(0, 11)
+
+TextLabel_7.Parent = Frame
+TextLabel_7.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel_7.BackgroundTransparency = 1
+TextLabel_7.BorderColor3 = Color3.new(0, 0, 0)
+TextLabel_7.BorderSizePixel = 0
+TextLabel_7.Position = UDim2.new(0.217391297, 0, 0.560975611, 0)
+TextLabel_7.Size = UDim2.new(0, 299, 0, 19)
+TextLabel_7.Font = Enum.Font.ArialBold
+TextLabel_7.Text = "Do you wish to continue?"
+TextLabel_7.TextColor3 = Color3.new(1, 1, 1)
+TextLabel_7.TextScaled = true
+TextLabel_7.TextSize = 14
+TextLabel_7.TextWrapped = true
+
+TextButton.Parent = Frame
+TextButton.BackgroundColor3 = Color3.new(0.0156863, 0.207843, 0.0313726)
+TextButton.BorderColor3 = Color3.new(0, 0, 0)
+TextButton.BorderSizePixel = 0
+TextButton.Position = UDim2.new(0.31379962, 0, 0.673780501, 0)
+TextButton.Size = UDim2.new(0, 73, 0, 50)
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = "Yes"
+TextButton.TextColor3 = Color3.new(0.333333, 1, 0)
+TextButton.TextSize = 44
+
+UICorner_3.Parent = TextButton
+UICorner_3.CornerRadius = UDim.new(0, 11)
+
+TextButton_2.Parent = Frame
+TextButton_2.BackgroundColor3 = Color3.new(0.290196, 0, 0)
+TextButton_2.BorderColor3 = Color3.new(0, 0, 0)
+TextButton_2.BorderSizePixel = 0
+TextButton_2.Position = UDim2.new(0.546313822, 0, 0.673780501, 0)
+TextButton_2.Size = UDim2.new(0, 73, 0, 50)
+TextButton_2.Font = Enum.Font.SourceSans
+TextButton_2.Text = "No"
+TextButton_2.TextColor3 = Color3.new(1, 0, 0)
+TextButton_2.TextScaled = true
+TextButton_2.TextSize = 14
+TextButton_2.TextWrapped = true
+
+UICorner_4.Parent = TextButton_2
+UICorner_4.CornerRadius = UDim.new(0, 11)
+
+-- Scripts
+
+local function RSNIQWQ_fake_script() -- TextButton.LocalScript 
+	local script = Instance.new('LocalScript', TextButton)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+		task.wait(.5)
+		loadstring(game:HttpGet('https://pastebin.com/raw/S4skyvLa'))()
+		script.Parent.Parent.Parent:Destroy()
+	end)
+end
+coroutine.wrap(RSNIQWQ_fake_script)()
+local function CEHGQW_fake_script() -- TextButton_2.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_2)
+
+	script.Parent.MouseButton1Click:Connect(function()
+		script.Parent.Parent.Parent:Destroy()
+	end)
+	
+end
+coroutine.wrap(CEHGQW_fake_script)()
